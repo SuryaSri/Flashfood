9926631548a1bddb50f0f87e36151bbe1e5bb02c
Surya Srikar Peruri <suryasrikar1997@gmail.com>
2018-03-17 01:29:43 +0530

    cart code added

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import bottle
 import json
 import googlemaps
-from bottle import get, post, request, run, install, response  # or route
+import random
+import pandas as pd
+from bottle import get, post, request, run, install, response , route
 from gevent import monkey; monkey.patch_all()
 from redis import Redis as red
 from pottery import RedisDict as redDict
@@ -29,6 +31,7 @@ class EnableCors(object):
 dir_con = red(host = '0.0.0.0',port = 6379,db = 0)
 #connection with pottery client
 pot_con = red.from_url('http://localhost:6379/')
+pot_arc = red.from_url('http://localhost:6379/1')
 
 rest_ids = NextId(key='rest-ids', masters={pot_con})
 
@@ -46,9 +49,8 @@ def byte2float(strung):
 	return float(byte2string(strung))
 
 def get_geocode(lat,longi):
-    gmaps = googlemaps.Client(key='AIzaSyCGIi0Ts6EavD1FN4Ckx0uR7Ikr1Z1Jwgw')
+    gmaps = googlemaps.Client(key='AIzaSyBKtTB8dxqymBPAnsfAr5sPWv-UTAWGZtY')
     reverse_geocode_result = gmaps.reverse_geocode((lat,longi))
-    #print('test')
     print(reverse_geocode_result[0]['formatted_address'])
     return reverse_geocode_result[0]['formatted_address']
 
@@ -60,7 +62,7 @@ User: pottery dictionary
 -lat
 -long
 -decoded_address
--
+-int confirmed_carts
 -int subscribed
 '''
 
@@ -70,6 +72,10 @@ def addUser():
     key = "user:" + str(body['ID']) + ":details"
     body.pop('ID')
     user = redDict(redis = pot_con, key = key)
+    arch = redDict(redis = pot_arc, key = key)
+    if 'decoded_address' not in arch:
+        arch = user
+        arch['confirmed_carts'] = 0
     for key in body:
         user[key] = body[key]
     if('subscribed' in user):
@@ -85,8 +91,6 @@ def addUser():
             body['location'] = 0
     print(json.dumps(body))
     yield json.dumps(body)
-#d = {'id':23,"name":"nihal"}
-#addUser(d)
 
 @post('/Saddress')
 def Saddress():
@@ -100,7 +104,6 @@ def Saddress():
 def getUser(UserId):
     key = "user:" + UserId + ":details"
     user = redDict(redis = pot_con, key = key)
-    #print (user)
     data = {}
     if(len(user)!=0):
         data['name'] = user['name']
@@ -129,6 +132,8 @@ def deleteUser(UserId):
     user = redDict(redis = pot_con, key = key)
     for keys in body:
         user.pop(keys)
+    print(json.dumps(body))
+    yield json.dumps(body)
 
 '''
 #/updateUser
@@ -140,9 +145,7 @@ def updateUser():
     for key in body:
 		if key != 'id':
 			user[key] = body[key]
-'''
 
-'''
 Restaurant: pottery dictionary
 -name
 -restId(primaryKey and artificial key)
@@ -151,24 +154,10 @@ Restaurant: pottery dictionary
 -long
 -delivery Radius
 -extra dets
-'''
 
-'''
-{"name":"New Punjabi Food Corner",
-"phone":963852741,
-"lat":12.844706
-"long":77.665583,
-"radius":5}
-{"name":"Hyderabadi Spice",
-"phone":963852741,
-"lat":12.8339903,
-"long":77.66395,
-"radius":5}
-{"name":"Domino's Pizza",
-"phone":963852741,
-"lat":12.844496,
-"long":77.665640,
-"radius":5}
+{"phone": 963852741, "long": 77.66564, "ID": 1, "name": "Domino's Pizza", "lat": 12.844496, "radius": 5}
+{"phone": 963852741, "long": 77.66395, "ID": 2, "name": "Hyderabadi Spice", "lat": 12.8339903, "radius": 5}
+{"phone": 963852741, "long": 77.665583, "ID": 3, "name": "New Punjabi Food Corner", "lat": 12.844706, "radius": 5}
 '''
 
 @post('/addrest')
@@ -176,18 +165,24 @@ def addRestaurant():
     body = request.json
     rest_id = next(rest_ids)
     key = "rest:"+str(rest_id)+":details"
-    rest = redDict(redis = pot_con, key = key)
+    rest = redDict(redis = pot_arc, key = key)
     rest['ID'] = rest_id
+    body['ID'] = rest_id
     for key in body:
         rest[key] = body[key]
+    print(json.dumps(body))
+    yield json.dumps(body)
 
 @post('/updaterest')
 def updateRestaurant():
     body = request.json
     key = "rest:"+str(body["ID"])+":details"
-    rest = redDict(redis = pot_con, key = key)
+    rest = redDict(redis = pot_arc, key = key)
     for key in body:
         rest[key] = body[key]
+    print(json.dumps(body))
+    yield json.dumps(body)
+
 '''
 Offers
 -id
@@ -198,14 +193,91 @@ Offers
 -dishName
 -originalPrice
 -offerPrice
+-link
 '''
+global OffersDB
+OffersDB = pd.DataFrame(data = {}, columns = ['ID','restID','dish','qty_sold','qty_left','type','originalPrice','offerPrice','link','restName'])
+
+@post('/addOffers')
+def addOffer():
+    global OffersDB
+    Offers = []
+    body = request.json
+    for i in range(len(body['data'])):
+        body['data'][i]['ID'] = random.randint(100,999)
+        key = "rest:"+str(body['data'][i]['restID'])+":details"
+        rest = redDict(redis = pot_arc, key = key)
+        body['data'][i]['restName'] = rest['name']
+        body['data'][i]['link'] = 'http://genii.ai/activebots/Babadadhaba/img/db/' + body['data'][i]['dish'].replace(" ","-") + ".jpg"
+        Offers.append(body['data'][i])
+    DB = pd.read_json(json.dumps(Offers),orient='records')
+    OffersDB = OffersDB.append(DB, ignore_index = True)
+
+@get('/getOffers/<sender>')
+def showoffers(sender):
+    global OffersDB
+    df = OffersDB.copy(deep = True)
+    df.drop(['restID','ID','type','qty_left','qty_sold'],axis = 1, inplace = True)
+    yield df.to_json(orient = 'records')
+
+@post('/claimOffer/<sender>')
+def claimOffer(sender):
+    global OffersDB
+    #print(OffersDB)
+    result = {'status':[],'remaining':[],'dish':[]}
+    body = request.json
+    body = str(body)
+    body = json.loads(body)
+    key = "user:"+ sender + ":details"
+    user = redDict(redis = pot_arc, key = key)
+    if('confirmed_carts' in user):
+        cart = redDict(redis = pot_con, key = "user:"+sender+":cart:"+str(user['confirmed_carts']+1))
+    else:
+        cart = redDict(redis = pot_con, key = "user:"+sender+":cart:1")
+        user['confirmed_carts'] = 0
+    for item in body:
+        if(OffersDB[OffersDB['dish']==item]['qty_left'].tolist()[0]>=int(body[item])):
+            cart[item] = body[item]
+            result['dish'].append(item)
+            result['status'].append('Yes')
+            result['remaining'].append(OffersDB[OffersDB['dish']==item]['qty_left'].tolist()[0])
+        else:
+            result['dish'].append(item)
+            result['status'].append('No')
+            result['remaining'].append(OffersDB[OffersDB['dish']==item]['qty_left'].tolist()[0])
+    yield json.dumps(result)
 
 
-
-def addOffer(body):
-'''
-def claimOffer(body):
-'''
+@get('/showCart/<sender>')
+def showcart(sender):
+    global OffersDB
+    #print(OffersDB)
+    user = redDict(redis = pot_arc, key = "user:" + sender + ":details")
+    if('confirmed_carts' in user):
+        cart = redDict(redis = pot_con, key = "user:"+sender+":cart:"+str(user['confirmed_carts']+1))
+    else:
+        cart = redDict(redis = pot_con, key = "user:"+sender+":cart:1")
+        user['confirmed_carts'] = 0
+    result = {'cart':'','status':[],'qty':[],'dish':[],'remaining':[]}
+    #print(result)
+    if(len(cart)!=0):
+        #print(cart)
+        result['cart'] = 'Yes'
+        for item in cart:
+            if((OffersDB[OffersDB['dish']==item]['qty_left'].tolist()[0])>=(int(cart[item]))):
+                result['dish'].append(item)
+                result['qty'].append(int(cart[item]))
+                result['status'].append('Yes')
+                result['remaining'].append(OffersDB[OffersDB['dish']==item]['qty_left'].tolist()[0])
+            else:
+                result['dish'].append(item)
+                result['qty'].append(int(cart[item]))
+                result['status'].append('No')
+                result['remaining'].append(OffersDB[OffersDB['dish']==item]['qty_left'].tolist()[0])
+        yield json.dumps(result)
+    else:
+        result['cart'] = 'No'
+        yield json.dumps(result)
 
 '''
 Orders
@@ -216,9 +288,6 @@ Orders
 -accompanyments
 -status
 -delivery boi
--
-
 '''
-
 install(EnableCors())
 run(host='0.0.0.0', port=3000, debug=True, server='gevent')
--- test.py
+++ test.py
@@ -1,36 +1,38 @@
 '''from bottle import get, post, request,run # or route
-
-
 @post('/login') # or @route('/login', method='POST')
 def do_login():
     username = request.json
     print(username,type(username))
     print (username['id'])
     return username
-
-run(host='0.0.0.0', port=4000, debug=True)
-
-
-'''
-import requests
-import json
-
-head = {'Content-Type': 'application/json'}
 r = requests.get(url="http://0.0.0.0:3000/getUser/1234567890",headers=head)
 res = r.json()
 if(res['name']==''):
     dta = {'ID': 1234567890, 'name': 'surya'}
-r = requests.post(url="http://0.0.0.0:3000/addUser",data = json.dumps(dta),headers=head)
 print ("surya",r.text)
 data = {'ID':1234567890, 'name':'surya','subscribed':1 }
 r1 = requests.post(url='http://0.0.0.0:3000/addUser',data = json.dumps(data),headers=head)
-
 data = r2.json()
 print (data)
 print(r2.status_code)
 print(r2.text)
+{"phone": 963852741, "long": 77.66564, "ID": 1, "name": "Domino's Pizza", "lat": 12.844496, "radius": 5}
+{"phone": 963852741, "long": 77.66395, "ID": 2, "name": "Hyderabadi Spice", "lat": 12.8339903, "radius": 5}
+{"phone": 963852741, "long": 77.665583, "ID": 3, "name": "New Punjabi Food Corner", "lat": 12.844706, "radius": 5}
+data = {"name":"New Punjabi Food Corner",
+"phone":963852741,
+"lat":12.844706,
+"long":77.665583,
+"radius":5}
+r = requests.post(url="http://0.0.0.0:3000/addrest",data = json.dumps(data),headers=head)
+'''
 
-
-data = {"1":{"ID":159487263,"name":"restA"
-             "offer":50, "dishes":{"dal":[1,100],"paneer":[2, 456]}
-             }}
+import requests
+import json
+head = {'Content-Type': 'application/json'}
+data = {"data":[{"restID":2,"qty_left":10,"qty_sold":0,"dish":"Chilly Chicken","originalPrice":120,"offerPrice":70,"type":"Starters[Non-Veg]"},
+{"restID":2,"qty_left":5,"qty_sold":0,"dish":"Pepper Chicken","originalPrice":120,"offerPrice":70,"type":"Starters[Non-Veg]"},
+{"restID":2,"qty_left":6,"qty_sold":0,"dish":"Andhra Chicken","originalPrice":130,"offerPrice":100,"type":"Starters[Non-Veg]"},
+{"restID":2,"qty_left":4,"qty_sold":0,"dish":"Chicken Biryani","originalPrice":130,"offerPrice":80,"type":"Biryani"},
+{"restID":2,"qty_left":8,"qty_sold":0,"dish":"Mutton Biryani","originalPrice":170,"offerPrice":100,"type":"Biryani"}]}
+r = requests.post(url="http://0.0.0.0:3000/addOffers",data = json.dumps(data),headers=head)
